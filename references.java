36ef7dfa622db47c190c181e70de3531ed607350
Varun Arora <varun.arora@vanderbilt.edu>
2019-01-23 14:47:16 -0600

    code tracing question, java

---
+++ references.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+class Solution {
+    static void makeLists(int N) {
+        ArrayList<List<Integer>> x = new ArrayList<>(); // [[]]
+        ArrayList<Integer> y = new ArrayList<>(Arrays.asList(1, 2, 3)); // [1, 2, 3]
+        x.add(y);
+        x.add(new ArrayList<>(Arrays.asList(3))); // Push [3] to x
+        x.add(new ArrayList<>(Arrays.asList(7))); // Push [7] to x
+
+        for (int i = 0; i < N; i++) {
+            var z = new ArrayList<>(Arrays.asList(N)); // [N]
+            if (i % 2 == 0) {
+                x.add(2, y); // Insert at index 2
+            }
+            else {
+                x.add(2, z); // Insert at index 2
+            }
+        }
+        y.add(0, 7); // Insert at index 0
+
+        x.get(0).add(5);
+        x.get(1).add(10);
+
+        System.out.println(y);
+        System.out.println(x);
+    }
+
+    public static void main(String[] args) {
+        makeLists(15);
+    }
+}
